c8a1645f96b8686da8a49c992818c85b0aa1a7de
Dettlaff404 <ashan.randima12ram@gmail.com>
2025-02-04 02:55:58 +0530

    Create BookDTO

--- src/main/java/lk/ijse/cmjd108/LibMgmt2025/controller/BookController.java
+++ src/main/java/lk/ijse/cmjd108/LibMgmt2025/controller/BookController.java
@@ -1,15 +1,22 @@
 package lk.ijse.cmjd108.LibMgmt2025.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/v1/books")
 public class BookController {
+
     @GetMapping("health")
     public String healthCheck(){
         return "Book Controller is Working";
+    }
+
+    @PostMapping
+    public void addBook(){
+
     }
 
 }
