c7a37152a382c0fcf8933a57d6dcf39fb8f5f11c
Dettlaff404 <ashan.randima12ram@gmail.com>
2025-02-04 00:51:22 +0530

    Initial commit

---
+++ src/test/java/lk/ijse/cmjd108/LibMgmt2025/LibMgmt2025ApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.ijse.cmjd108.LibMgmt2025;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibMgmt2025ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
