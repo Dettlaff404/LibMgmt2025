3e186ad941282a5e17c849437adc47943783f0f7
Dettlaff404 <ashan.randima12ram@gmail.com>
2025-02-04 19:15:36 +0530

    Add Get all books req

--- src/main/java/lk/ijse/cmjd108/LibMgmt2025/controller/BookController.java
+++ src/main/java/lk/ijse/cmjd108/LibMgmt2025/controller/BookController.java
@@ -6,6 +6,9 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @RestController
 @RequestMapping("/api/v1/books")
 public class BookController {
@@ -49,8 +52,80 @@ public class BookController {
                 "2025-10-10",
                 "15:30:00"
                 ));
-                
+    }
 
+    @GetMapping
+    public ResponseEntity<List<BookDTO>> getAllBooks() {
+        List<BookDTO> bookDTOList = new ArrayList<>();
+
+        bookDTOList.add(new BookDTO(
+                "B001",
+                "Java Basics",
+                "John Doe",
+                "1st Edition",
+                "Tech Press",
+                "978-1111111111",
+                1500.00,
+                50,
+                30,
+                "2025-01-15",
+                "10:00:00"
+        ));
+
+        bookDTOList.add(new BookDTO(
+                "B002",
+                "Advanced Java",
+                "Jane Smith",
+                "3rd Edition",
+                "Code House",
+                "978-2222222222",
+                2500.00,
+                40,
+                35,
+                "2025-02-20",
+                "09:30:00"
+        ));
+
+        bookDTOList.add(new BookDTO(
+                "B003",
+                "Spring Framework",
+                "Alan Walker",
+                "2nd Edition",
+                "Springer Publications",
+                "978-3333333333",
+                3000.00, 30,
+                25,
+                "2025-03-10",
+                "11:45:00"
+        ));
+
+        bookDTOList.add(new BookDTO(
+                "B004",
+                "Microservices Design",
+                "Clara Adams",
+                "1st Edition",
+                "Microtech Press",
+                "978-4444444444",
+                4000.00, 20,
+                15,
+                "2025-04-05",
+                "14:20:00"
+        ));
+
+        bookDTOList.add(new BookDTO("B005",
+                "Database Management",
+                "Chris Martin",
+                "4th Edition",
+                "DB Books",
+                "978-5555555555",
+                3500.00,
+                25,
+                20,
+                "2025-05-15",
+                "16:10:00"
+        ));
+
+        return ResponseEntity.ok(bookDTOList);
     }
 
 }
