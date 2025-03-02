7329d3da64fa10e9d6b9cfb0067a9b1a30d82ce4
Ashan Randima Manamperi <120651088+Dettlaff404@users.noreply.github.com>
2025-03-02 23:10:16 +0530

    Add mappings for member handling

--- src/main/java/lk/ijse/cmjd108/LibMgmt2025/util/EntityDTOConvert.java
+++ src/main/java/lk/ijse/cmjd108/LibMgmt2025/util/EntityDTOConvert.java
@@ -1,7 +1,9 @@
 package lk.ijse.cmjd108.LibMgmt2025.util;
 
 import lk.ijse.cmjd108.LibMgmt2025.dto.BookDTO;
+import lk.ijse.cmjd108.LibMgmt2025.dto.MemberDTO;
 import lk.ijse.cmjd108.LibMgmt2025.entities.BookEntity;
+import lk.ijse.cmjd108.LibMgmt2025.entities.MemberEntity;
 import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
 import org.modelmapper.TypeToken;
@@ -23,5 +25,16 @@ public class EntityDTOConvert {
     }
     public List<BookDTO> toBookDTOList(List<BookEntity> bookEntities){
         return modelMapper.map(bookEntities,new TypeToken<List<BookDTO>>(){}.getType());
+    }
+
+    //Member
+    public MemberEntity convertMemberDTOToMemberEntity(MemberDTO memberDTO){
+        return modelMapper.map(memberDTO, MemberEntity.class);
+    }
+    public MemberDTO convertMemberEntityToMemberDTO(MemberEntity memberEntity){
+        return modelMapper.map(memberEntity, MemberDTO.class);
+    }
+    public List<MemberDTO> toMemberDTOList(List<MemberEntity> memberEntities){
+        return modelMapper.map(memberEntities,new TypeToken<List<MemberDTO>>(){}.getType());
     }
 }
