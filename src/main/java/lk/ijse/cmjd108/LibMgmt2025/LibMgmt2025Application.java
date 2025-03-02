ab34872c45cfcb5f63b55fad2cf763b10a77c85b
Ashan Randima Manamperi <120651088+Dettlaff404@users.noreply.github.com>
2025-03-02 18:58:05 +0530

    Add ModelMapper to the application

--- src/main/java/lk/ijse/cmjd108/LibMgmt2025/LibMgmt2025Application.java
+++ src/main/java/lk/ijse/cmjd108/LibMgmt2025/LibMgmt2025Application.java
@@ -1,10 +1,17 @@
 package lk.ijse.cmjd108.LibMgmt2025;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class LibMgmt2025Application {
+
+	@Bean
+	public ModelMapper modelMapper(){
+		return new ModelMapper();
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(LibMgmt2025Application.class, args);
