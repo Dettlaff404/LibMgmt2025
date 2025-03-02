9cc30a1dd3c986c6f83b673174f8c2d3a1d9c353
Ashan Randima Manamperi <120651088+Dettlaff404@users.noreply.github.com>
2025-03-02 21:04:41 +0530

    Save Book tested

--- src/main/java/lk/ijse/cmjd108/LibMgmt2025/service/impl/BookServiceIMPL.java
+++ src/main/java/lk/ijse/cmjd108/LibMgmt2025/service/impl/BookServiceIMPL.java
@@ -1,15 +1,23 @@
 package lk.ijse.cmjd108.LibMgmt2025.service.impl;
 
+import jakarta.transaction.Transactional;
+import lk.ijse.cmjd108.LibMgmt2025.dao.BookDao;
 import lk.ijse.cmjd108.LibMgmt2025.dto.BookDTO;
 import lk.ijse.cmjd108.LibMgmt2025.service.BookService;
+import lk.ijse.cmjd108.LibMgmt2025.util.EntityDTOConvert;
 import lk.ijse.cmjd108.LibMgmt2025.util.UtilData;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-
-import java.util.ArrayList;
 import java.util.List;
 
 @Service
+@RequiredArgsConstructor
+@Transactional
 public class BookServiceIMPL implements BookService {
+
+    private final BookDao bookDao;
+    private final EntityDTOConvert entityDTOConvert;
+
     @Override
     public void addBook(BookDTO bookDTO) {
         //Business Process
@@ -17,6 +25,7 @@ public class BookServiceIMPL implements BookService {
         bookDTO.setLastUpdateDate(UtilData.generateTodayDate());
         bookDTO.setLastUpdateTime(UtilData.genrateCurrentTime());
         //Pass to Dao
+        bookDao.save(entityDTOConvert.convertBookDTOToBookEntity(bookDTO));
     }
 
     @Override
