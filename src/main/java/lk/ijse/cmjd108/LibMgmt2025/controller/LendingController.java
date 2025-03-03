3108e85bc55763edd29e9bfe992cd3179aa9f6bf
Ashan Randima Manamperi <120651088+Dettlaff404@users.noreply.github.com>
2025-03-03 17:58:06 +0530

    Handle Lending books and test the logic - Update the status code for not available books

--- src/main/java/lk/ijse/cmjd108/LibMgmt2025/controller/LendingController.java
+++ src/main/java/lk/ijse/cmjd108/LibMgmt2025/controller/LendingController.java
@@ -32,7 +32,7 @@ public class LendingController {
             return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
         }catch (DataPersistException | EnoughBooksNotFoundException e) {
             e.printStackTrace();
-            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+            return new ResponseEntity<>(HttpStatus.CONFLICT);
         } catch (Exception e) {
             e.printStackTrace();
             return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
