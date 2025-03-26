b889a49689160eb57cc82243a3c78b29c11853c5
Ashan Randima Manamperi <120651088+Dettlaff404@users.noreply.github.com>
2025-03-26 12:45:55 +0530

    Add Authorization to the application

--- src/main/java/lk/ijse/cmjd108/LibMgmt2025/controller/BookController.java
+++ src/main/java/lk/ijse/cmjd108/LibMgmt2025/controller/BookController.java
@@ -9,6 +9,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
@@ -82,6 +83,7 @@ public class  BookController {
     }
 
     @GetMapping("getallbooks")
+    @PreAuthorize("hasRole('ADMIN')")
     public ResponseEntity<List<BookDTO>> getAllBooks() {
         return ResponseEntity.ok(bookService.getAllBooks());
     }
